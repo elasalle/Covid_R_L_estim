72db156c2b3d767657daf83a89d2ee657addb258
juliana-du <juliana.du@ens-lyon.fr>
2024-03-19 14:45:59 +0100

    adding spatial corr. to R & delta choice display

--- display/display_figures_by_county.py
+++ display/display_figures_by_county.py
@@ -1,21 +1,90 @@
+import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from include.load_data.load_graph_examples import example_choice, set_Graph_fromMatrix
+from include.load_data.load_graph_examples import connectStruct_choice, set_Graph_fromMatrix
+
+from include.build_synth import deltaGrid as dG
+from include.build_synth.Tikhonov_method import Tikhonov_spat_corr
+from include.build_synth.compute_spatCorrLevels import compute_spatCorrLevels
 
 
 def display_connect_structure(example):
     """
     Displays the connectivity structure named 'example'.
-    :param example: str between 'Line', 'Hub', ... described in include/load_data/load_graph_examples.py
-    :return:
+    :param example: str to choose the connectivity structure (see include/build_synth/load_graph_examples.py)
     """
-    depCont, pos, labels, colorMap = example_choice(example)
-    G_graph = set_Graph_fromMatrix(depCont)
+    strucMat, pos, labels, colorMap = connectStruct_choice(example)
+    G_graph = set_Graph_fromMatrix(strucMat)
 
-    fig5, axG = plt.subplots(figsize=(3.75, 2.45))
-    fig5.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
+    fig, axG = plt.subplots(figsize=(3.75, 2.45))
+    fig.subplots_adjust(wspace=0, hspace=0, left=0, right=1, bottom=0, top=1)
 
     nx.draw(G_graph, ax=axG, labels=labels, node_color=colorMap, pos=pos, node_size=500, font_size=20)
     axG.axis("off")
-    fig5.show()
+    fig.show()
+
+
+def spatCorrLevels(R_by_county, B_matrix):
+    """
+    Displays the evolution of the spatial regularization term when considering large logarithmic scale of delta.
+    :param R_by_county:
+    :param B_matrix:
+    """
+
+    nbDeps, days = np.shape(R_by_county)
+
+    # Computation of deltaGrid through updating the delta_min, delta_max computation
+    delta_min, delta_max, powerMin, powerMax = dG.compute_delta_withG(R_by_county, B_matrix, fileSuffix='Last')
+
+    nbDelta = 500
+    if powerMin > 0:
+        deltaGrid = np.round(np.logspace(powerMin, powerMax, num=nbDelta), int(powerMin))
+    else:
+        deltaGrid = np.round(np.logspace(powerMin, powerMax, num=nbDelta), - int(powerMin) + 1)
+
+    RDiff = np.zeros((nbDelta, nbDeps, days))
+
+    spatialRegNorml2 = np.zeros(nbDelta)
+
+    for k in range(len(deltaGrid)):
+        delta = deltaGrid[k]
+        RDiff[k] = Tikhonov_spat_corr(R_by_county, B_matrix, delta)
+
+        # Spatial regularization term (coordinate-wise norm 2)
+        spatialRegNorml2[k] = np.sum(np.abs(np.dot(B_matrix, RDiff[k])) ** 2)
+
+    delta_I, delta_II, delta_III, delta_IV = compute_spatCorrLevels(R_by_county, B_matrix, fileSuffix='Last')
+
+    lineStyleCorrLevels = {'0': 'solid',
+                           'I': 'solid',
+                           'II': 'dashed',
+                           'III': 'dashdot',
+                           'IV': 'dotted'}
+
+    plt.rc('text', usetex=True)
+    plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
+    plt.style.use('display/matlabMatchingStyles.mplstyle')
+
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 4.9))
+    fig.tight_layout(pad=7.5)
+    fig.subplots_adjust(hspace=0.3, left=0.08, right=0.99, bottom=0.22, top=0.88)
+    plt.ticklabel_format(axis="both", style="sci", scilimits=(0, 0))
+
+    start = len(deltaGrid[deltaGrid < 10 ** (-4)])
+    ax.semilogx(deltaGrid[start:], spatialRegNorml2[start:], color='black')
+    corrLevel2delta = {'I': delta_I,
+                       'II': delta_II,
+                       'III': delta_III,
+                       'IV': delta_IV}
+    for corrLevel in ['I', 'II', 'III', 'IV']:
+        ax.vlines([corrLevel2delta[corrLevel]], 0, np.max(spatialRegNorml2),
+                  linestyles=lineStyleCorrLevels[corrLevel], label='$\delta_\mathtt{%s}$' % corrLevel, color='black')
+    ax.legend(loc=(0.83, 0.075))
+
+    # Formatting the grid
+    ax.grid(which="major")
+    ax.set_ylabel('$\lVert \mathsf{G}\\boldsymbol{\mathsf{R}}^\star\\rVert^2_2$')
+    ax.set_xlabel('$\delta$', labelpad=0, loc='center')
+    ax.set_title('Evolution of spatial regularization term')
+    fig.show()
---
+++ include/build_synth/compute_spatCorrLevels.py
@@ -0,0 +1,43 @@
+from scipy.io import savemat, loadmat
+from include.build_synth import deltaGrid as dG
+
+namesDiff = {'NoDiffusion': '0',
+             'Low': '\delta_\mathtt{I}',
+             'Medium': '\delta_\mathtt{II}',
+             'High': '\delta_\mathtt{III}',
+             'VeryHigh': '\delta_\mathtt{IV}',
+             'SuperHigh': '\delta_\mathtt{V}'}
+
+
+def compute_spatCorrLevels(R_by_county, B_matrix, fileSuffix='Last', saveData=True):
+    """
+    :param R_by_county:
+    :param B_matrix:
+    :param fileSuffix:
+    :param saveData:
+    :return:
+    """
+
+    # Computation delta_min, delta_max computation
+    delta_min, delta_max, powerMin, powerMax = \
+        dG.compute_delta_withG(R_by_county, B_matrix, fileSuffix=fileSuffix)
+
+    fileInit = loadmat("data/Synthetic/Multivariate/Line_graph/spatCorrLevels_Line.mat", squeeze_me=True)
+    delta_I = fileInit['deltaSLow']/fileInit['deltaSmin'] * delta_min
+    delta_II = fileInit['deltaSMedium'] / fileInit['deltaSmin'] * delta_min
+    delta_III = fileInit['deltaSHigh'] / fileInit['deltaSmin'] * delta_min
+    delta_IV = fileInit['deltaSVeryHigh'] / fileInit['deltaSmin'] * delta_min
+
+    if saveData:
+        savemat("include/build_synth/deltaFiles/diffusionLevels%s.mat" % fileSuffix,
+                {'delta_I': delta_I,
+                 'delta_II': delta_II,
+                 'delta_III': delta_III,
+                 'delta_IV': delta_IV,
+                 'B_matrix': B_matrix,
+                 'R_by_county': R_by_county,
+                 'name': fileSuffix})
+
+    return delta_I, delta_II, delta_III, delta_IV
+
+
